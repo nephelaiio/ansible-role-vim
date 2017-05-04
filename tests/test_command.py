b53eb5ee700404ae10d5f3492c98f8993823e9f8
Ted Cook <teodoro.cook@gmail.com>
2017-05-03 21:40:46 -0600

    Fix lint warnings

--- tests/test_command.py
+++ tests/test_command.py
@@ -2,7 +2,7 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 
 
 testinfra_hosts = \
-        AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
+    AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
 
 
 def test_command(Command):
